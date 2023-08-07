6ffc43ff0f3fa86d28a1bbaac97363a654fc117d
yzays8 <yzays8@gmail.com>
2023-08-08 00:53:34 +0900

    Add error handling to scripts

--- test.sh
+++ test.sh
@@ -2,6 +2,10 @@
 
 readonly OUT="testc"
 
-make run FILE=$1 > $OUT.c
+make runc FILE=$1 > $OUT.c
+if [ $? -ne 0 ]; then
+    echo "Failed to compile $1"
+    exit 1
+fi
 gcc -o $OUT $OUT.c
 ./"$OUT"
--- test64.sh
+++ test64.sh
@@ -3,5 +3,9 @@
 readonly OUT="test64"
 
 make run FILE=$1 > $OUT.s
+if [ $? -ne 0 ]; then
+    echo "Failed to compile $1"
+    exit 1
+fi
 gcc -static $OUT.s -o $OUT
 ./"$OUT"
