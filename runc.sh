76010f34861d0b3906585db228f26d66ff037b2a
yzays8 <yzays8@gmail.com>
2023-12-27 09:03:50 +0900

    Fix scripts

--- runc.sh
+++ runc.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+set -eu
+cd $(dirname $0)
+
 readonly OUT="testc"
 
 make runc FILE=$1 > $OUT.c
-if [ $? -ne 0 ]; then
-    echo "Failed to compile $1"
-    exit 1
-fi
 gcc -o $OUT $OUT.c
 ./"$OUT"
